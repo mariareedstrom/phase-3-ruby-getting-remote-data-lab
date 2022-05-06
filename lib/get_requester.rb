27fbf4d8a63ad64d6a56e656f65d16495b2383d8
Maria Reedstrom <maria.reedstrom@gmail.com>
2022-05-06 11:10:03 -0500

    commit tree 07a2e7b79071bb9c7a33a5abe8c1e361fd3b43e0

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,26 @@
 # Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+  # URL =  "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+
+
+  def initialize(url)
+    @url = URI.parse(url)
+  end
+
+
+  def get_response_body
+    response = Net::HTTP.get_response(@url)
+    response.body
+  end
+
+  def parse_json
+    JSON.parse(get_response_body)
+  end
+
+
+end
+
